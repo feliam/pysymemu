a0e305b1594e099ef0f615de790ac8605107df4a
feliam <feliam@binamuse.com>
2014-07-29 13:07:16 -0300

    Testing via setup.py now works (pythont setup.py test)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "PySymEmu",
+    name = "pysymemu",
     version = "0.0.1-alpha",
     author = "Felipe Andres Manzano",
     author_email = "feliam@binamuse.com",
@@ -17,7 +17,6 @@ setup(
     download_url= 'http://github.com/',
     platforms = ['linux', 'win32', 'win64'],
     keywords = "testing reverse enginering symbolic execution white box fuzzing automatic test case generation",
-    packages=['PySymEmu', 'tests'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -32,6 +31,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Quality Assurance",
     ],
+    test_suite="test",
 )
 
 
