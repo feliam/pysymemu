1ae1671877f605d0896d3d784eaabe4de3310734
feliam <feliam@binamuse.com>
2015-04-21 14:42:33 -0300

    mktests ported to capstone

--- test/auto/mk_tests.py
+++ test/auto/mk_tests.py
@@ -1,9 +1,12 @@
 import copy
+import traceback
 import sys
 import sys
 import time
 import subprocess
-from distorm3 import Decompose, Decode16Bits, Decode32Bits, Decode64Bits, Mnemonics, Registers
+from capstone import *
+from capstone.x86 import *
+CapRegisters = ['(INVALID)', 'AH', 'AL', 'AX', 'BH', 'BL', 'BP', 'BPL', 'BX', 'CH', 'CL', 'CS', 'CX', 'DH', 'DI', 'DIL', 'DL', 'DS', 'DX', 'EAX', 'EBP', 'EBX', 'ECX', 'EDI', 'EDX', 'RFLAGS', 'EIP', 'EIZ', 'ES', 'ESI', 'ESP', 'FPSW', 'FS', 'GS', 'IP', 'RAX', 'RBP', 'RBX', 'RCX', 'RDI', 'RDX', 'RIP', 'RIZ', 'RSI', 'RSP', 'SI', 'SIL', 'SP', 'SPL', 'SS', 'CR0', 'CR1', 'CR2', 'CR3', 'CR4', 'CR5', 'CR6', 'CR7', 'CR8', 'CR9', 'CR10', 'CR11', 'CR12', 'CR13', 'CR14', 'CR15', 'DR0', 'DR1', 'DR2', 'DR3', 'DR4', 'DR5', 'DR6', 'DR7', 'FP0', 'FP1', 'FP2', 'FP3', 'FP4', 'FP5', 'FP6', 'FP7', 'K0', 'K1', 'K2', 'K3', 'K4', 'K5', 'K6', 'K7', 'MM0', 'MM1', 'MM2', 'MM3', 'MM4', 'MM5', 'MM6', 'MM7', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'ST0', 'ST1', 'ST2', 'ST3', 'ST4', 'ST5', 'ST6', 'ST7', 'XMM0', 'XMM1', 'XMM2', 'XMM3', 'XMM4', 'XMM5', 'XMM6', 'XMM7', 'XMM8', 'XMM9', 'XMM10', 'XMM11', 'XMM12', 'XMM13', 'XMM14', 'XMM15', 'XMM16', 'XMM17', 'XMM18', 'XMM19', 'XMM20', 'XMM21', 'XMM22', 'XMM23', 'XMM24', 'XMM25', 'XMM26', 'XMM27', 'XMM28', 'XMM29', 'XMM30', 'XMM31', 'YMM0', 'YMM1', 'YMM2', 'YMM3', 'YMM4', 'YMM5', 'YMM6', 'YMM7', 'YMM8', 'YMM9', 'YMM10', 'YMM11', 'YMM12', 'YMM13', 'YMM14', 'YMM15', 'YMM16', 'YMM17', 'YMM18', 'YMM19', 'YMM20', 'YMM21', 'YMM22', 'YMM23', 'YMM24', 'YMM25', 'YMM26', 'YMM27', 'YMM28', 'YMM29', 'YMM30', 'YMM31', 'ZMM0', 'ZMM1', 'ZMM2', 'ZMM3', 'ZMM4', 'ZMM5', 'ZMM6', 'ZMM7', 'ZMM8', 'ZMM9', 'ZMM10', 'ZMM11', 'ZMM12', 'ZMM13', 'ZMM14', 'ZMM15', 'ZMM16', 'ZMM17', 'ZMM18', 'ZMM19', 'ZMM20', 'ZMM21', 'ZMM22', 'ZMM23', 'ZMM24', 'ZMM25', 'ZMM26', 'ZMM27', 'ZMM28', 'ZMM29', 'ZMM30', 'ZMM31', 'R8B', 'R9B', 'R10B', 'R11B', 'R12B', 'R13B', 'R14B', 'R15B', 'R8D', 'R9D', 'R10D', 'R11D', 'R12D', 'R13D', 'R14D', 'R15D', 'R8W', 'R9W', 'R10W', 'R11W', 'R12W', 'R13W', 'R14W', 'R15W']
 
 count = 0
  
@@ -121,18 +124,30 @@ while True:
         pc = gdb.getR({'i386': 'EIP', 'amd64': 'RIP'}[arch]) 
         text = ''.join([chr(gdb.getByte(pc+i)) for i in range(16)])
         #print text.encode('hex')
-        instruction = Decompose(pc, text, {'i386':Decode32Bits, 'amd64':Decode64Bits}[arch])[0]
-        #print instruction
-        print "#INSTRUCTION:",hex(pc), instruction
+        #instruction = Decompose(pc, text, {'i386':Decode32Bits, 'amd64':Decode64Bits}[arch])[0]
 
-        if instruction.mnemonic in ['CPUID', 'RDTSC', 'NOP', 'SYSCALL', 'INT', 'SYSENTER'] or \
-           instruction.segment != 255 and not instruction.isSegmentDefault:
-            print "#Skiping:, ", instruction.mnemonic
+        cap_arch = {'i386': CS_ARCH_X86, 'amd64': CS_ARCH_X86}[arch] 
+        cap_mode = {'i386': CS_MODE_32, 'amd64': CS_MODE_64}[arch] 
+        md = Cs(cap_arch, cap_mode)
+        md.detail = True
+        md.syntax = 0
+
+        instruction = None
+        for i in md.disasm(text, pc):
+            instruction = i
+            break
+
+        #print instruction
+        disassembly = "0x%x:\t%s\t%s" %(instruction.address, instruction.mnemonic, instruction.op_str)
+        print "#INSTRUCTION:", disassembly
+
+        if instruction.insn_name().upper() in ['CPUID', 'RDTSC', 'NOP', 'SYSCALL', 'INT', 'SYSENTER']:
+            print "#Skiping:, ", instruction.insn_name().upper()
             print "SKP:", gdb.stepi()
             continue
 
         # gather PRE info
-        test = {'mnemonic': instruction.mnemonic, 'disassembly': str(instruction), 'text':text[:instruction.size], 'arch':arch}
+        test = {'mnemonic': instruction.insn_name().upper(), 'disassembly': disassembly, 'text':text[:instruction.size], 'arch':arch}
         registers = {}
         memory = {}
         #default registers...
@@ -149,33 +164,35 @@ while True:
 
         #save operands
         for o in instruction.operands:
-            if o.type == 'Immediate':
+            if o.type == X86_OP_IMM:
                 #ignore, already encoded in instruction
                 continue
-            elif o.type == 'Register':
-                registers[o.name] = gdb.getR(o.name)
-            elif o.type == 'AbsoluteMemory':
+            elif o.type == X86_OP_REG:
+                REGNAME = instruction.reg_name(o.reg).upper()
+                registers[REGNAME] = gdb.getR(REGNAME)
+            elif o.type == X86_OP_MEM:
                 #save register involved and memory values
                 address = 0
-                address += o.disp
-                if not o.base is None:
-                    name = Registers[o.base]
+                address += o.mem.disp
+                if o.mem.base != 0:
+                    base = instruction.reg_name(o.mem.base).upper()
+                    registers[base] = gdb.getR(base)
+                    address += registers[base]
+                if o.mem.index != 0:
+                    name = instruction.reg_name(o.mem.index).upper()
                     registers[name] = gdb.getR(name)
-                    address += registers[name]
-                if not o.index is None and o.scale > 0:
-                    name = Registers[o.index]
-                    registers[name] = gdb.getR(name)
-                    address += o.scale*registers[name]
+                    address += o.mem.scale*registers[name]
+
                 for i in range(address, address+o.size):
                     memory[i] = chr(gdb.getByte(i))
 
-        if instruction.mnemonic in STACK_INSTRUCTIONS:
+        if instruction.insn_name().upper() in STACK_INSTRUCTIONS:
             #save a bunch of stack
             pointer =  gdb.getR({'i386': 'ESP', 'amd64': 'RSP'}[arch])
             for i in range(-{'i386': 5, 'amd64': 9}[arch], {'i386': 5, 'amd64': 9}[arch]):
                 memory[pointer+i] = chr(gdb.getByte(pointer+i))
 
-        if instruction.mnemonic in ['LEAVE', 'ENTER']:
+        if instruction.insn_name().upper() in ['LEAVE', 'ENTER']:
             pointer =  gdb.getR({'i386': 'EBP', 'amd64': 'RBP'}[arch])
             for i in range(-{'i386': 5, 'amd64': 9}[arch], {'i386': 5, 'amd64': 9}[arch]):
                 memory[pointer+i] = chr(gdb.getByte(pointer+i))
@@ -207,24 +224,27 @@ while True:
         registers[{'i386': 'EAX', 'amd64': 'RAX'}[arch]] = gdb.getR({'i386': 'EAX', 'amd64': 'RAX'}[arch])
         registers[{'i386': 'EFLAGS', 'amd64': 'RFLAGS'}[arch]] = gdb.getR('EFLAGS')
 
+
+
+
         #save operands
         for o in instruction.operands:
-            if o.type == 'Immediate':
-                #ignore, already encoded in instruction
+            if o.type == X86_OP_REG:
+                name = instruction.reg_name(o.reg).upper()
+                registers[name] = gdb.getR(name)
+            elif o.type == X86_OP_IMM:
                 continue
-            elif o.type == 'Register':
-                registers[o.name] = gdb.getR(o.name)
 
         #update memory
         for i in memory.keys():
             memory[i] = chr(gdb.getByte(i))
 
-        if instruction.mnemonic in STACK_INSTRUCTIONS:
+        if instruction.insn_name().upper() in STACK_INSTRUCTIONS:
             #save a bunch of stack
             pointer =  gdb.getR({'i386': 'ESP', 'amd64': 'RSP'}[arch])
             for i in range(-{'i386': 5, 'amd64': 9}[arch], {'i386': 5, 'amd64': 9}[arch]):
                 memory[pointer+i] = chr(gdb.getByte(pointer+i))
-        if instruction.mnemonic in ['LEAVE', 'ENTER']:
+        if instruction.insn_name().upper() in ['LEAVE', 'ENTER']:
             pointer =  gdb.getR({'i386': 'EBP', 'amd64': 'RBP'}[arch])
             for i in range(-{'i386': 5, 'amd64': 9}[arch], {'i386': 5, 'amd64': 9}[arch]):
                 memory[pointer+i] = chr(gdb.getByte(pointer+i))
@@ -237,7 +257,7 @@ while True:
 
         count += 1
         #check if exit
-        if instruction.mnemonic in ['SYSCALL', 'INT', 'SYSENTER']:
+        if instruction.insn_name().upper() in ['SYSCALL', 'INT', 'SYSENTER']:
             if "The program has no registers now." in gdb.correspond("info registers \n"):
                 print "done" 
                 break
@@ -245,7 +265,13 @@ while True:
     except Exception,e:
         if "The program has no registers now." in gdb.correspond("info registers\n"):
             break
-        print "#", e, instruction
+        print '-'*60
+        traceback.print_exc(file=sys.stdout)
+        print '-'*60
+        import pdb
+        pdb.set_trace()
+        print "#", e
+        print "instruction", dir(instruction)
         if not stepped:
             gdb.stepi()
 
